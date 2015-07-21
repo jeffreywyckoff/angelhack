e477cd98beea2b108061ba675c362cf02c05cb12
jeffreywyckoff <jeffreywyckoff@gmail.com>
2015-07-21 11:08:37 -0400

    first commit

---
+++ both/router/meta.js
@@ -0,0 +1,9 @@
+if(Meteor.isClient) {
+  Meta.config({
+      options: {
+        // Meteor.settings[Meteor.settings.environment].public.meta.title
+        title: '#AH8',
+        suffix: '#AH8'
+      }
+  });
+}
